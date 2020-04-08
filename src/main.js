1cffef31e79d3aee86ae78bb3ea44565f532c3ef
tx <302871586@qq.com>
2020-04-08 22:44:58 +0800

    搭建商城基本路由，以及接口代理

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
 import App from './App.vue'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Cart from './pages/Cart.vue'
+import Address from './pages/Address.vue'
+Vue.use(Router);
+export default new Router({
+  routes: [
+    {
+      name: 'Cart',
+      path: '/',
+      component: Cart
+    },
+    {
+      name: 'address',
+      path: '/address',
+      component: Address
+    }
+  ]
+})
